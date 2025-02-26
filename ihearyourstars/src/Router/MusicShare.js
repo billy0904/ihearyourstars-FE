c622306727108a7746b1a8d8d5b0eb504b421af0
Gabin Lee <gb94@ewhain.net>
2025-02-26 20:11:51 +0900

    💄 오르골 공유 페이지 퍼블리싱

--- ihearyourstars/src/Router/MusicShare.js
+++ ihearyourstars/src/Router/MusicShare.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 import Button from "../components/Button";
+import { ReactComponent as CircleBg } from "../img/common/circle_bg.svg";
+import { ReactComponent as Orbit } from "../img/MusicBox/orbit.svg";
+
+function MusicShare() {
+  const musicTitle = "집에가고싶다";
+
+  return (
+    <MusicShareDiv>
+      <Contents>
+        <Title>
+          <h1>{musicTitle}를</h1>
+          <h1>간직하세요</h1>
+        </Title>
+        <CircleBg />
+      </Contents>
+      <Button text="링크 복사하기"></Button>
+      <Button text="나가기"></Button>
+    </MusicShareDiv>
+  );
+}
 
 const MusicShareDiv = styled.div`
   padding: 30px 0;
@@ -7,21 +27,30 @@ const MusicShareDiv = styled.div`
     display: block;
     width: 100%;
     background-color: transparent;
-    color: white;
-    font-weight: 500;
-    font-size: 30px;
   }
 `;
 
-function MusicShare() {
-  return (
-    <MusicShareDiv>
-      <span>"집에 가고 싶다" 를</span>
-      <span>간직하세요</span>
-      <Button text="링크 복사하기"></Button>
-      <Button text="나가기"></Button>
-    </MusicShareDiv>
-  );
-}
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  color: white;
+  font-weight: 400;
+  line-height: 40px;
+  font-size: 32px;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 47px;
+  margin-bottom: 62px;
+
+  `;
 
 export default MusicShare;
