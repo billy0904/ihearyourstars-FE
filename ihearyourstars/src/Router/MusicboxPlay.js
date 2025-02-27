a0cdc25cb925361a1f41184d48e74b1a68451688
Gabin Lee <gb94@ewhain.net>
2025-02-27 21:42:53 +0900

    💄 오르골 손잡이 회전 구현

--- ihearyourstars/src/Router/MusicboxPlay.js
+++ ihearyourstars/src/Router/MusicboxPlay.js
@@ -1,24 +1,19 @@
 import styled, { keyframes } from "styled-components";
+import { MusicBox } from '../components/MusicBox';
 import { ReactComponent as Body } from "../img/MusicBox/musicbox_play.svg"
 import { ReactComponent as Star } from "../img/MusicBox/musicbox_star.svg"
 import { ReactComponent as Handle } from "../img/MusicBox/musicbox_handle.svg"
 
 function MusicboxPlay() {
+
+
   return (
     <MusicboxPlayDiv>
       <Title>
         <h1>♪ 집에가고싶다</h1>
       </Title>
       <Notes />
-      <MusicBox>
-        <StarDiv>
-          <Star />
-        </StarDiv>
-        <HandleDiv>
-          <Handle />
-        </HandleDiv>
-        <Body />
-      </MusicBox>
+      <MusicBox />
       <span>손잡이를 돌려 음악을 재생해보세요</span>
     </MusicboxPlayDiv>
   );
@@ -56,31 +51,4 @@ const Title = styled.div`
 
 const Notes = styled.div`
   height: 470px;
-`;
-
-const MusicBox = styled.div`
-  position: relative;
-  width: 430px;
-`;
-
-const floatAnimation = keyframes`
-  0% { transform: translateY(0); }
-  50% { transform: translateY(-7%); }
-  100% { transform: translateY(0); }
-`;
-
-const StarDiv = styled.div`
-  position: absolute;
-  top: -16%;
-  left: 23%;
-  z-index: 1;
-  animation: ${floatAnimation} 3s infinite ease-in-out;
-`;
-
-const HandleDiv = styled.div`
-  position: absolute;
-  top: 37%;
-  left: 44%;
-  z-index: 2;
-  cursor: pointer;
 `;
