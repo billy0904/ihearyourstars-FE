989d10f617fec5f1b6243dbd2fb597a6da49fd81
Gamddalki <hannahk01@naver.com>
2025-03-01 14:40:15 +0900

    💄 로딩 화면에서만 그라데이션 애니메이션 배경 적용

--- ihearyourstars/src/App.js
+++ ihearyourstars/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./Router/Home";
 import Footer from "./components/Footer";
 import BodyWrapper from "./components/BodyWrapper";
@@ -10,24 +15,33 @@ import MusicPlay from "./Router/MusicPlay";
 import MusicName from "./Router/MusicName";
 import MusicShare from "./Router/MusicShare";
 
+function MainContent() {
+  const location = useLocation();
+  const isLoading = location.pathname === "/loading";
+
+  return (
+    <BodyWrapper isLoading={isLoading}>
+      <Routes>
+        <Route path="/music/share" element={<MusicShare />} />
+        <Route path="/music/name" element={<MusicName />} />
+        <Route path="/music/play" element={<MusicPlay />} />
+        <Route path="/music" element={<Music />} />
+
+        <Route path="/musicbox/play" element={<MusicboxPlay />} />
+        <Route path="/musicbox" element={<Musicbox />} />
+
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BodyWrapper>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <BodyWrapper>
-        <Routes>
-          <Route path="/music/share" element={<MusicShare />} />
-          <Route path="/music/name" element={<MusicName />} />
-          <Route path="/music/play" element={<MusicPlay />} />
-          <Route path="/music" element={<Music />} />
-
-          <Route path="/musicbox/play" element={<MusicboxPlay />} />
-          <Route path="/musicbox" element={<Musicbox />} />
-
-          <Route path="/loading" element={<Loading />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </BodyWrapper>
+      <MainContent />
     </Router>
   );
 }
