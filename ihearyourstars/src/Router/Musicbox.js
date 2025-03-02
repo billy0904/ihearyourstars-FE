4c2d33b9755bf25a487748558a8ed15f91c4c83e
Gamddalki <hannahk01@naver.com>
2025-03-02 22:55:41 +0900

    💄 공유 후 화면 오르골 명 UI 수정

--- ihearyourstars/src/Router/Musicbox.js
+++ ihearyourstars/src/Router/Musicbox.js
@@ -1,28 +1,28 @@
 import styled from "styled-components";
 import Button from "../components/Button";
 import { ReactComponent as CircleBg } from "../img/common/circle_bg.svg";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Musicbox() {
   const userName = "가빈";
-  const musicTitle = "집에가고싶다";
+  const musicTitle = "안녕하시렵니까별들";
 
   const nav = useNavigate();
 
   const handleNavigateHome = () => {
-    nav('/');
-  }
+    nav("/");
+  };
 
   return (
     <MusicboxTitleDiv>
       <Contents>
-              <Title>
-                <h1>{userName} 님의 오르골</h1>
-                <h1>"{musicTitle}"</h1>
-              </Title>
-              <CircleBg />
-              <Text>오르골을 눌러 노래를 들어보세요</Text>
-            </Contents>
+        <Title>
+          <h2>{userName} 님의 오르골</h2>
+          <h1>{musicTitle}</h1>
+        </Title>
+        <CircleBg />
+        <Text>오르골을 눌러 노래를 들어보세요</Text>
+      </Contents>
       <Button text="나도 만들기" onClick={handleNavigateHome}></Button>
     </MusicboxTitleDiv>
   );
@@ -44,15 +44,10 @@ const Title = styled.div`
   justify-content: center;
   align-items: center;
   gap: 16px;
-
-  color: white;
-  font-weight: 400;
-  line-height: 40px;
-  font-size: 32px;
 `;
 
 const Text = styled.div`
-  color: rgba(0, 0, 0, 0.50);
+  color: rgba(0, 0, 0, 0.5);
   font-weight: 400;
   line-height: 20px;
   font-size: 15px;
@@ -65,7 +60,6 @@ const Contents = styled.div`
   align-items: center;
   gap: 47px;
   margin-bottom: 62px;
-
 `;
 
 export default Musicbox;
